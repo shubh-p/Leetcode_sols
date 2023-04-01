ae3942e7d5ab6fe30f341b8de329b113baeca92f
Shubhlaxh Porwal <shubhlaxh.porwal2020@vitstudent.ac.in>
2023-04-02 00:07:42 +0530

    Time: 483 ms (5.06%), Space: 17.2 MB (12.15%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,8 +1,17 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        sum=0
-        for i,a in enumerate(nums):
+        count=[0]*(2*(10**4) + 1)
+        for i in nums:
+            count[i+10000]+=1
+        for j in range(1,len(count)):
+            count[j]+=count[j-1]
+        ans=[0]*len(nums)
+        for i in range(len(nums)-1,-1,-1):
+            count[nums[i]+10000]-=1
+            ans[count[nums[i]+10000]]=nums[i]
+        print(ans)
+        s=0
+        for i,a in enumerate(ans):
             if i%2==0:
-                sum+=a
-        return sum
+                s+=a
+        return s
