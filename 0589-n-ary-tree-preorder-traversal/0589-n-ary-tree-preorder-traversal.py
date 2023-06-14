2a122dfab9d41c105655dd91e954d2b9725d8551
Shubhlaxh Porwal <shubhlaxh.porwal2020@vitstudent.ac.in>
2023-06-14 12:38:11 +0530

    Time: 73 ms (10.86%), Space: 18.6 MB (16.24%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        ans=[]
+        def dfs(node):
+            if not node:
+                return
+            ans.append(node.val)
+            for node in node.children:
+                dfs(node)
+                
+        dfs(root)
+        return ans
