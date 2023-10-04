b201381ec1d8dda34c34b100025e4dde86ecee78
Shubhlaxh Porwal <shubhlaxh.porwal2020@vitstudent.ac.in>
2023-10-04 20:11:41 +0530

    Time: 779 ms (86.48%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT A.id FROM Weather A ,Weather B WHERE A.temperature > B.temperature AND DATEDIFF(A.recordDate,B.recordDate)=1;
+SELECT A.id FROM Weather A INNER JOIN Weather B WHERE A.temperature > B.temperature AND DATEDIFF(A.recordDate,B.recordDate)=1;
