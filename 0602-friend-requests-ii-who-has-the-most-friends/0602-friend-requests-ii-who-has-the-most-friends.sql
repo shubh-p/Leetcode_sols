f5b33c294574074fda041a880f1b0d410f6a57ec
Shubhlaxh Porwal <shubhlaxh.porwal2020@vitstudent.ac.in>
2023-10-11 23:42:41 +0530

    Time: 482 ms (92.24%), Space: 0B (100.00%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+with base as(select requester_id id from RequestAccepted
+union all
+select accepter_id id from RequestAccepted)
+
+
+select id, count(*) num  from base group by 1 order by 2 desc limit 1
